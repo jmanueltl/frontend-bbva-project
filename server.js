4d0732ba8269a18f011ddacfc5ffe8c496674373
Juan Tipismana <jmanueltl@gmail.com>
2019-01-17 00:23:20 +0100

    deploy vf1

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.use("/styles", express.static(__dirname));
 app.use("/images", express.static(__dirname + '/images'));
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
